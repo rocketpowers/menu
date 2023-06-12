366812cad7e69a79dac7c6c737c05ff38a379874
rocketpowers <rocketpowerswillian@gmail.com>
2023-06-11 21:33:57 -0300

    backend init

---
+++ backend/src/main/java/Packages/controller/FoodController.java
@@ -0,0 +1,20 @@
+package Packages.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("food")
+@RestController
+public class FoodController {
+	
+	
+	@GetMapping
+	public void getAll() {
+		
+		
+		
+	}
+	
+
+}
